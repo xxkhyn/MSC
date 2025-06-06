f6dcd90b86904e03654cade49d42bc4ac73a2bfb
numata <s0623038@iwate-u.ac.jp>
2025-06-06 13:23:36 +0900

    変更

--- MSC/urls.py
+++ MSC/urls.py
@@ -7,4 +7,5 @@ app_name = 'MSC'
 urlpatterns = [
   path('', views.index_view, name="index"),
   path('api/condition/submit/', views.condition_submit_api, name='condition_submit_api'),
+  path('api/score/calculate/', views.calculate_score_api, name='calculate_score_api'),
 ]
--- MSC/views.py
+++ MSC/views.py
@@ -3,6 +3,7 @@ from .forms import ConditionForm, HandForm
 from .models import Condition, Hand, ScoreResult
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from MSC.logic import calculator
 import json
 
 def index_view(request):
@@ -50,6 +51,31 @@ def hand_input_view(request):
         form = HandForm()
 
     return render(request, 'MSC/hand.html', {'form': form})
+
+@csrf_exempt
+def calculate_score_api(request):
+    if request.method == 'POST':
+        try:
+            hand = Hand.objects.last()
+            condition = Condition.objects.last()
+            result = calculator.calculate_score(hand, condition)
+
+            return JsonResponse({
+                "success": True,
+                "result": {
+                    "han": result.han,
+                    "fu": result.fu,
+                    "point": result.point,
+                    "yaku_list": result.yaku_list,
+                    "error_message": result.error_message,
+                }
+            })
+
+        except Exception as e:
+            return JsonResponse({"success": False, "error": str(e)}, status=500)
+
+    return JsonResponse({"success": False, "error": "Invalid request"}, status=405)
+
 
 def score_result_view(request, result_id):
     result = get_object_or_404(ScoreResult, pk=result_id)
